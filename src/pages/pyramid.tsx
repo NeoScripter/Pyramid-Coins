8950cdba7af49c693325712ac847508fe26a36e4
Ilya <sange0337@gmail.com>
2025-04-27 18:34:45 +0800

    fix flying coin animation

--- src/pages/pyramid.tsx
+++ src/pages/pyramid.tsx
@@ -13,11 +13,16 @@ import { cc } from '@/utils/cc';
 type PyramidProps = {
     resetGame: () => void;
 };
+
+/* const DISAPPEARING_TIME = 1000 * 60 * 2; */
+const DISAPPEARING_TIME = 10000;
+
 export default function Pyramid({ resetGame }: PyramidProps) {
     const [openScroll, setOpenScroll] = useState(false);
     const [flipAll, setFlipAll] = useState(false);
     const [canAnimate, setCanAnimate] = useState(true);
     const selectedCoinRef = useRef<HTMLDivElement>(null);
+    const selectedImageRef = useRef<HTMLImageElement>(null);
     const [animatedCoinPosition, setAnimatedCoinPosition] =
         useState<CoinCopyProps | null>(null);
     const [resetCount, setResetCount] = useState(0);
@@ -36,7 +41,15 @@ export default function Pyramid({ resetGame }: PyramidProps) {
         setTimeout(() => {
             setResetCount((prev) => prev + 1);
             setCanAnimate(true);
+            setImage('');
         }, 500);
+    }
+
+    function setImage(imageSrc: string) {
+        const imageElement = selectedImageRef?.current;
+        if (!imageElement) return;
+
+        imageElement.src = imageSrc;
     }
 
     const rows = useMemo(() => {
@@ -84,14 +97,17 @@ export default function Pyramid({ resetGame }: PyramidProps) {
         setTimeout(() => {
             setFlipAll(true);
             setOpenScroll(true);
+
+            setImage(image);
+            setAnimatedCoinPosition(null);
         }, 3500);
 
         setTimeout(() => {
             setIsAppearing(false);
-        }, 9500);
+        }, DISAPPEARING_TIME - 500);
         setTimeout(() => {
             reset();
-        }, 1000 * 60 * 2);
+        }, DISAPPEARING_TIME);
     }
 
     return (
@@ -127,8 +143,15 @@ export default function Pyramid({ resetGame }: PyramidProps) {
                     <div>
                         <div
                             ref={selectedCoinRef}
-                            className="size-62.75 relative top-16 mx-auto aspect-square rounded-full"
-                        ></div>
+                            className="size-62.75 relative top-4 mx-auto aspect-square rounded-full"
+                        >
+                            <img
+                                ref={selectedImageRef}
+                                src=""
+                                alt=""
+                                className="w-full h-full object-center object-cover rounded-full"
+                            />
+                        </div>
                     </div>
                     <Scroll isOpen={openScroll} winningText={prize || ''} />
                 </div>
